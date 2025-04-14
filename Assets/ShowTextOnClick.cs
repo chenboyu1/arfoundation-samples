02bad55b694fec6205dc0b557fb97c4e491523de
陳柏妤 <147235033+chenboyu1@users.noreply.github.com>
2025-04-14 12:48:59 +0800

    調整

--- Assets/ShowTextOnClick.cs
+++ Assets/ShowTextOnClick.cs
@@ -4,16 +4,15 @@ using UnityEngine.UI;
 
 public class ShowTextOnClick : MonoBehaviour
 {
-    public TMP_Text displayText; // 指定要顯示的文字
+    public TMP_Text displayText;
 
     void Start()
     {
-        displayText.gameObject.SetActive(false); // 一開始隱藏文字
+        displayText.gameObject.SetActive(false);
     }
 
     public void ToggleText()
     {
-        // 如果現在是顯示狀態，按一下就隱藏；反之亦然
         displayText.gameObject.SetActive(!displayText.gameObject.activeSelf);
     }
 }
