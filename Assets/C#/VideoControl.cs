a59c7ef742968f5bb411eba6d187c8cb9385842a
陳柏妤 <147235033+chenboyu1@users.noreply.github.com>
2025-05-06 17:46:19 +0800

    新增動畫按鈕

--- Assets/C#/VideoControl.cs
+++ Assets/C#/VideoControl.cs
@@ -1,13 +1,74 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
-public class VideoControl : MonoBehaviour
+public class VideoToggle : MonoBehaviour
 {
-    public VideoPlayer videoPlayer;
+    [System.Serializable]
+    public class VideoGroup
+    {
+        public string name;
+        public VideoPlayer videoPlayer;
+        public RawImage rawImage;
+        [HideInInspector] public bool isPlaying = false;
+    }
+
+    public VideoGroup[] videoGroups;
+
+    private int currentPlayingIndex = -1;
 
     void Start()
     {
-        videoPlayer.Play();
+        // 一開始全部隱藏並停止
+        foreach (var group in videoGroups)
+        {
+            group.videoPlayer.Stop();
+            group.rawImage.enabled = false;
+            group.isPlaying = false;
+        }
+    }
+
+    public void ToggleVideoByIndex(int index)
+    {
+        if (index < 0 || index >= videoGroups.Length)
+        {
+            Debug.LogWarning("無效的影片索引");
+            return;
+        }
+
+        // 如果點到的是正在播放的影片，則關閉它
+        if (videoGroups[index].isPlaying)
+        {
+            StopVideo(index);
+            currentPlayingIndex = -1;
+        }
+        else
+        {
+            // 關閉其他播放中的影片
+            if (currentPlayingIndex != -1)
+            {
+                StopVideo(currentPlayingIndex);
+            }
+
+            // 播放選取的影片
+            PlayVideo(index);
+            currentPlayingIndex = index;
+        }
+    }
+
+    private void PlayVideo(int index)
+    {
+        var group = videoGroups[index];
+        group.rawImage.enabled = true;
+        group.videoPlayer.Play();
+        group.isPlaying = true;
+    }
+
+    private void StopVideo(int index)
+    {
+        var group = videoGroups[index];
+        group.videoPlayer.Stop();
+        group.rawImage.enabled = false;
+        group.isPlaying = false;
     }
 }
-
