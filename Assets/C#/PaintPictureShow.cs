c6889dddfb237ff99e4e9579973859355bdbcddd
Lee-Raining <146429173+Lee-Raining@users.noreply.github.com>
2025-09-01 17:52:40 +0800

    文字修完!

--- Assets/C#/PaintPictureShow.cs
+++ Assets/C#/PaintPictureShow.cs
@@ -16,17 +16,5 @@ public class ToggleImage : MonoBehaviour
         isVisible = !isVisible;
         imageObject.SetActive(isVisible);
 
-        // 額外：取得圖片尺寸 
-        Image img = imageObject.GetComponent<Image>();
-        if (img != null && img.sprite != null)
-        {
-            int width = img.sprite.texture.width;
-            int height = img.sprite.texture.height;
-            Debug.Log("圖片尺寸：寬 = " + width + "，高 = " + height);
-        }
-        else
-        {
-            Debug.LogWarning("圖片尚未設定 Sprite 或 Image 組件未找到");
-        }
     }
 }
