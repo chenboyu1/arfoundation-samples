8cb029de46dfcb08cdd764714a5e071c5a4a7837
陳柏妤 <147235033+chenboyu1@users.noreply.github.com>
2026-02-05 13:49:55 +0800

    修正code

--- Assets/showCV.cs
+++ Assets/showCV.cs
@@ -103,7 +103,7 @@ public class showCV : MonoBehaviour
             currentPage++;
             currentPageEN++;
 
-            yield return StartCoroutine(WaitWhilePaused(12f));
+            yield return new WaitForSeconds(12f);
         }
     }
 
